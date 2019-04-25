79fb2a6596ed7afa69d95b9faa7c91f7ff55e2d7
Akshay Shenvi <akshayshenvi@gmail.com>
2019-04-25 14:11:46 -0500

    to start of Multiple Linear Regression

---
+++ Regression/Multiple Linear Regression/multipleLinearRegression.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 22:46:37 2019
+
+@author: aksha
+"""
+
+# Multiple Linear Regression
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('50_Startups.csv')
+
+
